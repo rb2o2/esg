41aa6beda53e117a4332135d951d35655b4a4ed7
Roman Bortnikow <rb2o2.dev@gmail.com>
2025-12-18 19:36:20 +0300

    * reformat

--- clj/src/esg/plot.clj
+++ clj/src/esg/plot.clj
@@ -1,7 +1,9 @@
 (ns esg.plot
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io])
-  (:import [tech.tablesaw.plotly Plot] [tech.tablesaw.plotly.components Figure]))
+  (:import [tech.tablesaw.plotly Plot] 
+           [tech.tablesaw.plotly.components Figure]
+           [tech.tablesaw.plotly.traces HeatmapTrace]))
 (def data
   (with-open [file (io/reader (nth *command-line-args* 0))]
     (-> file
@@ -11,9 +13,15 @@
 (Plot/show
  (Figure.
   (into-array
-   tech.tablesaw.plotly.traces.HeatmapTrace
-   [(.build (tech.tablesaw.plotly.traces.HeatmapTrace/builder
+   HeatmapTrace
+   [(.build (HeatmapTrace/builder
              (into-array Object (range 0 100))
              (into-array Object (range 0 100))
-             (into-array (map #(double-array (map Double/parseDouble (let [length (count %)] (take (- length 1) %)))) (take (- (count data) 1) data)))) 
-            )])))
+             (into-array 
+              (map 
+               #(double-array 
+                 (map Double/parseDouble 
+                      (let [length (count %)] (take (- length 1) %)))) 
+               (take (- (count data) 1) data)))) 
+            )]
+   )))
---
+++ clj/src/esg/plot_hansolo.clj
@@ -0,0 +1,9 @@
+(ns esg.plot-hansolo
+  (:import [eu.hansolo.fx.charts MatrixPane]))
+
+(comment
+  ;; try out different libs for heatmap charts,
+  ;; preferably with vector field overlay
+  
+  
+  )
