e2811ecd2a4decd9ed9a4b73b1e50d9e2556c82f
Roman Bortnikow <rb2o2.dev@gmail.com>
2025-12-28 02:44:53 +0300

    * modify code move to fns

--- clj/src/esg/core.clj
+++ clj/src/esg/core.clj
@@ -2,14 +2,26 @@
   (:import [io.github.rb2o2.esg Mesh2D])
   (:require (clojure.string)) 
   )
+(comment (def data (let [mesh (Mesh2D. (Integer/parseInt (nth *command-line-args* 0))
+                               (Integer/parseInt (nth *command-line-args* 1))
+                               (Integer/parseInt (nth *command-line-args* 2)))]
+             (.initMoves mesh (java.util.List/of
+                               (into-array Double [0.4 0.35 1.0])
+                               (into-array Double [0.1 0.83 0.5])))
+             (Mesh2D/csvPrint (.uvalues mesh)))))
 
-(spit (str "../data/" (.toString (java.util.UUID/randomUUID)) ".csv") 
-      (let [mesh (Mesh2D. (Integer/parseInt (nth *command-line-args* 0)) 
-                          (Integer/parseInt (nth *command-line-args* 1)) 
-                          (Integer/parseInt (nth *command-line-args* 2)))] 
-        (.initMoves mesh (java.util.List/of 
-                          (into-array Double [0.4 0.35 1.0]) 
-                          (into-array Double [0.1 0.83 0.5])
-                          )
-                    )
-        (Mesh2D/csvPrint (.uvalues mesh))))
+(defn data-points [nx ny nf]
+  (fn [c & charges]
+    (let [mesh (Mesh2D. nx ny nf)]
+      (print (partition  3 (cons c charges)))
+      (.initMoves mesh (apply java.util.List/of 
+                        (map (fn [x3] (into-array Double x3))
+                             (partition 3 (cons c charges))
+                             ))
+                  )
+      (Mesh2D/csvPrint (.uvalues mesh)))
+    )
+  )
+
+;; (spit (str "../data/" (.toString (java.util.UUID/randomUUID)) ".csv") data)
+;; (print ((data-points 64 64 64) 0.18 0.95 1.0 0.34 0.52 1.0 0.78 0.65 1.0))
--- clj/src/esg/plot.clj
+++ clj/src/esg/plot.clj
@@ -4,23 +4,43 @@
   (:import [tech.tablesaw.plotly Plot] 
            [tech.tablesaw.plotly.components Figure]
            [tech.tablesaw.plotly.traces HeatmapTrace]))
-(def data
-  (with-open [file (io/reader (nth *command-line-args* 0))]
-    (-> file
-        (slurp)
-        (csv/read-csv))))
-(Plot/show
- (Figure.
-  (into-array
-   HeatmapTrace
-   [(.build (HeatmapTrace/builder
-             (into-array Object (range 0 (count data)))
-             (into-array Object (range 0 (count (nth data 0))))
-             (into-array 
-              (map 
-               #(double-array 
-                 (map Double/parseDouble 
-                      (let [length (count %)] (take (- length 1) %)))) 
-               (take (- (count data) 1) data)))) 
-            )]
-   )))
+(comment (def data
+           (with-open [file (io/reader (nth *command-line-args* 0))]
+             (-> file
+                 (slurp)
+                 (csv/read-csv)))))
+(defn read-data [slurped]
+  (-> slurped
+      (csv/read-csv)))
+
+
+(comment
+  (Plot/show
+   (Figure.
+    (into-array
+     HeatmapTrace
+     [(.build (HeatmapTrace/builder
+               (into-array Object (range 0 (count data)))
+               (into-array Object (range 0 (count (nth data 0))))
+               (into-array
+                (map
+                 #(double-array
+                   (map Double/parseDouble
+                        (let [length (count %)] (take (- length 1) %))))
+                 (take (- (count data) 1) data)))))])))
+  )
+
+(defn plot-data [read] 
+ (Plot/show
+  (Figure.
+   (into-array
+    HeatmapTrace
+    [(.build (HeatmapTrace/builder
+              (into-array Object (range 0 (count read)))
+              (into-array Object (range 0 (count (nth read 0))))
+              (into-array
+               (map
+                #(double-array
+                  (map Double/parseDouble
+                       (let [length (count %)] (take (- length 1) %))))
+                (take (- (count read) 1) read)))))]))) )
