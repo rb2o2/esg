05884586f652ab762befbbcfd65deb9b2fdfeebe
Roman Bortnikow <rb2o2.dev@gmail.com>
2025-12-28 05:12:43 +0300

    + add update mesh by a single move + prettify clj

--- clj/src/esg/plot.clj
+++ clj/src/esg/plot.clj
@@ -1,6 +1,7 @@
 (ns esg.plot
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [esg.core :as core])
   (:import [tech.tablesaw.plotly Plot] 
            [tech.tablesaw.plotly.components Figure]
            [tech.tablesaw.plotly.traces HeatmapTrace]))
@@ -20,8 +21,8 @@
     (into-array
      HeatmapTrace
      [(.build (HeatmapTrace/builder
-               (into-array Object (range 0 (count data)))
-               (into-array Object (range 0 (count (nth data 0))))
+               (into-array Object (range 0.0 (/ 1.0 (count data))))
+               (into-array Object (range 0.0 (/ 1.0 (count (nth data 0)))))
                (into-array
                 (map
                  #(double-array
@@ -36,11 +37,23 @@
    (into-array
     HeatmapTrace
     [(.build (HeatmapTrace/builder
-              (into-array Object (range 0 (count read)))
-              (into-array Object (range 0 (count (nth read 0))))
+              (into-array Object 
+                          (map str 
+                               (range                                 
+                                0.0 1.0 (/ 1.0 (count read)))
+                               ))
+              (into-array Object 
+                          (map str 
+                               (range 
+                                0.0 1.0 (/ 1.0 (count (nth read 0))))
+                               ))
               (into-array
                (map
                 #(double-array
-                  (map Double/parseDouble
-                       (let [length (count %)] (take (- length 1) %))))
-                (take (- (count read) 1) read)))))]))) )
+                  (map Double/parseDouble %
+                       ))
+                read))))]))) )
+
+(let [d (read-data ((core/data-points 100 100 100) 0.18 0.95 1.0 0.34 0.52 1.0 0.78 0.65 1.0))] 
+    (plot-data d))
+(Thread/sleep 800)
