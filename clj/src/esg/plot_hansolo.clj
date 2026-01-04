703b9a72b39ac7921b4a66b5352a393309681899
Roman Bortnikow <rb2o2.dev@gmail.com>
2026-01-04 08:22:58 +0300

    - han solo plot client in clojure + clean up pom

--- clj/src/esg/plot_hansolo.clj
+++
@@ -1,9 +0,0 @@
-(ns esg.plot-hansolo
-  (:import [eu.hansolo.fx.charts MatrixPane]))
-
-(comment
-  ;; try out different libs for heatmap charts,
-  ;; preferably with vector field overlay
-  
-  
-  )
